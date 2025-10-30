50b7b4a92114f1c92bb7f99c79e0c634876a0c4b
Ralitsa Isaeva <raliisaeva@gmail.com>
2025-10-31 00:38:44 +0200

    finished with the beginning of the game, up to the part where the user will read the instructions (added some buttons)

--- style.js
+++ style.js
@@ -1,14 +1,47 @@
 //Output game starting text
-window.addEventListener("load", function () {
-    const text = "Well, seems like you did it. You broke into the system. Congratulations!\nYou thought that was all? Oh, you're so wrong. The game is just now beginning... \nYou think that you're the best hacker out there? Now's your chance to prove it...";
-    const speed = 100;
-    let i = 0;
+document.getElementById("start-button").addEventListener("click", function () {
+    const button = this;
+    setTimeout(() => button.style.display = "none", 500);
+
     const textElement = document.getElementById("text");
-    function typeEffect() {
-        if (i < text.length) {
-            textElement.textContent += text.charAt(i);
+    document.getElementById("blinking-cursor").style.opacity = "1";
+    this.style.display = "none";
+
+    const messages = [
+        "Welcome, hacker...",
+        "You did it. You've broken into the system and you've entered the Break The Code Challenge.",
+        "You thought that was all? Oh, you're so wrong... The game is just beginning...",
+        "Get ready to solve the impossible and to prove that you're the best hacker out there..."
+    ];
+    const typingSpeed = 100;
+    const pauseBetweenParts = 1200;
+    var currPart = 0;
+    var i = 0;
+    button.style.opacity = "0";
+    function typeEffect()
+    {
+        const currMessage = messages[currPart];
+        if (i < currMessage.length)
+        {
+            textElement.textContent += currMessage.charAt(i);
             i++;
-            setTimeout(typeEffect, speed);
+            setTimeout(typeEffect, typingSpeed);
+        }
+        else
+        {
+            currPart++;
+            if(currPart < messages.length)
+            {
+                setTimeout(() => {
+                    textElement.textContent += "\n\n";
+                    i = 0;
+                    typeEffect();
+                }, pauseBetweenParts);
+            }
+            else
+            {
+                document.getElementById("continue-button").style.opacity = "1";
+            }
         }
     }
     typeEffect();
